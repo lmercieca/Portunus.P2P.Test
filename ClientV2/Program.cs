b4aef8af2abf464b623617c57903c0189fdbe2fc
Loui Mercieca <louimercieca@hotmail.com>
2019-03-05 11:27:42 +0100

    Main Commit

---
+++ ClientV2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientV2
+{
+    class Program
+    {
+        const int PORT_NO = 5002;
+        const string SERVER_IP = "127.0.0.1";
+
+        const int CLIENT_PORT_NO = 5001;
+        const string CLIENT_SERVER_IP = "127.0.0.1";
+
+
+        static void Main(string[] args)
+        {
+            //---data to send to the server---
+            string textToSend = DateTime.Now.ToString();
+
+            // Receive
+            UdpClient listener = new UdpClient(PORT_NO);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, PORT_NO);
+            string received_data;
+            byte[] receive_byte_array;
+            receive_byte_array = listener.Receive(ref groupEP);
+            Console.WriteLine("Received a broadcast from {0}", groupEP.ToString());
+            received_data = Encoding.ASCII.GetString(receive_byte_array, 0, receive_byte_array.Length);
+            Console.WriteLine("data follows \n{0}\n\n", received_data);
+
+
+            //Send
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress send_to_server_address = IPAddress.Parse(CLIENT_SERVER_IP);
+            IPEndPoint sending_server_end_point = new IPEndPoint(send_to_server_address, CLIENT_PORT_NO);
+
+            byte[] send_server_buffer = Encoding.ASCII.GetBytes("Hello from client 2");
+            server.SendTo(send_server_buffer, sending_server_end_point);
+
+
+            /*        
+            // Send to client
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress send_to_address = IPAddress.Parse(CLIENT_SERVER_IP);
+            IPEndPoint sending_end_point = new IPEndPoint(send_to_address, CLIENT_PORT_NO);
+
+            byte[] send_buffer = Encoding.ASCII.GetBytes("Hello from client 2 Just To you");
+            client.SendTo(send_buffer, sending_end_point);
+            */
+
+        }
+    }
+}
---
+++ Interceptor/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interceptor
+{
+    class Program
+    {
+        const int PORT_NO_CLIENT = 5001;
+        const int PORT_NO_SERVER = 5002;
+        const string SERVER_IP = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+
+            TcpListener listenerFromClient = new TcpListener(localAdd, PORT_NO_CLIENT);
+            TcpListener listenerFromServer = new TcpListener(localAdd, PORT_NO_SERVER);
+
+            Console.WriteLine("Interceptor Listening...");
+            listenerFromClient.Start();
+            listenerFromServer.Start();
+
+            //---incoming client connected---
+            TcpClient client = listenerFromClient.AcceptTcpClient();
+            TcpClient server = listenerFromServer.AcceptTcpClient();
+
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStreamClient = client.GetStream();
+            NetworkStream nwStreamServer = server.GetStream();
+
+
+            byte[] bufferClient = new byte[client.ReceiveBufferSize];
+            byte[] bufferServer = new byte[server.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesReadClient = nwStreamClient.Read(bufferClient, 0, client.ReceiveBufferSize);
+            int bytesReadServer = nwStreamServer.Read(bufferServer, 0, server.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceivedFromClient = Encoding.ASCII.GetString(bufferClient, 0, bytesReadClient) + " from client";
+            string dataReceivedFromServer = Encoding.ASCII.GetString(bufferServer, 0, bytesReadServer) + " from server";
+
+            Console.WriteLine("Received : " + dataReceivedFromClient);
+            Console.WriteLine("Received : " + dataReceivedFromServer);
+
+            //---write back the text to the client---
+            Console.WriteLine("Sending back to server: " + dataReceivedFromClient);
+            Console.WriteLine("Sending back to client: " + dataReceivedFromServer);
+
+            nwStreamClient.Write(bufferServer, 0, bytesReadServer);
+            nwStreamServer.Write(bufferClient, 0, bytesReadClient);
+
+            client.Close();
+            server.Close();
+
+            listenerFromClient.Stop();
+            listenerFromServer.Stop();
+
+            Console.ReadLine();
+        }
+    }
+}
